e87a854d18d82adae34eacf288e9df66b3d69b96
Valera1978 <valera1978@tut.by>
2018-02-19 12:45:35 +0300

    update vendor for camera\ril

--- gtaxllte-vendor-blobs.mk
+++ gtaxllte-vendor-blobs.mk
@@ -51,21 +51,21 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/bdwlan30.bin:system/etc/firmware/bdwlan30.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/bdwlan32.bin:system/etc/firmware/bdwlan32.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/Data.msc:system/etc/firmware/Data.msc \
+    vendor/samsung/gtaxllte/proprietary/etc/firmware/nvm_tlv.bin:system/etc/firmware/nvm_tlv.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/nvm_tlv_1.3.bin:system/etc/firmware/nvm_tlv_1.3.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/nvm_tlv_2.1.bin:system/etc/firmware/nvm_tlv_2.1.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/nvm_tlv_3.0.bin:system/etc/firmware/nvm_tlv_3.0.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/nvm_tlv_3.2.bin:system/etc/firmware/nvm_tlv_3.2.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/nvm_tlv_tf_1.1.bin:system/etc/firmware/nvm_tlv_tf_1.1.bin \
-    vendor/samsung/gtaxllte/proprietary/etc/firmware/nvm_tlv.bin:system/etc/firmware/nvm_tlv.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/otp30.bin:system/etc/firmware/otp30.bin \
-    vendor/samsung/gtaxllte/proprietary/etc/firmware/qwlan30_ibss.bin:system/etc/firmware/qwlan30_ibss.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/qwlan30.bin:system/etc/firmware/qwlan30.bin \
+    vendor/samsung/gtaxllte/proprietary/etc/firmware/qwlan30_ibss.bin:system/etc/firmware/qwlan30_ibss.bin \
+    vendor/samsung/gtaxllte/proprietary/etc/firmware/rampatch_tlv.img:system/etc/firmware/rampatch_tlv.img \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/rampatch_tlv_1.3.tlv:system/etc/firmware/rampatch_tlv_1.3.tlv \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/rampatch_tlv_2.1.tlv:system/etc/firmware/rampatch_tlv_2.1.tlv \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/rampatch_tlv_3.0.tlv:system/etc/firmware/rampatch_tlv_3.0.tlv \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/rampatch_tlv_3.2.tlv:system/etc/firmware/rampatch_tlv_3.2.tlv \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/rampatch_tlv_tf_1.1.tlv:system/etc/firmware/rampatch_tlv_tf_1.1.tlv \
-    vendor/samsung/gtaxllte/proprietary/etc/firmware/rampatch_tlv.img:system/etc/firmware/rampatch_tlv.img \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/utf30.bin:system/etc/firmware/utf30.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/utfbd30.bin:system/etc/firmware/utfbd30.bin \
     vendor/samsung/gtaxllte/proprietary/etc/firmware/utfbd32.bin:system/etc/firmware/utfbd32.bin \
@@ -78,7 +78,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtaxllte/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg \
     vendor/samsung/gtaxllte/proprietary/lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
     vendor/samsung/gtaxllte/proprietary/lib/hw/audio.primary.universal7870.so:system/lib/hw/audio.primary.universal7870.so \
-    vendor/samsung/gtaxllte/proprietary/lib/hw/camera.universal7870.so:system/lib/hw/camera.universal7870.so \
     vendor/samsung/gtaxllte/proprietary/lib/hw/gps.default.so:system/lib/hw/gps.default.so \
     vendor/samsung/gtaxllte/proprietary/lib/hw/gralloc.exynos5.so:system/lib/hw/gralloc.exynos5.so \
     vendor/samsung/gtaxllte/proprietary/lib/hw/sensors.universal7870.so:system/lib/hw/sensors.universal7870.so \
@@ -87,32 +86,41 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtaxllte/proprietary/lib/lib_SoundAlive_SRC384_ver300.so:system/lib/lib_SoundAlive_SRC384_ver300.so \
     vendor/samsung/gtaxllte/proprietary/lib/lib_soundaliveresampler.so:system/lib/lib_soundaliveresampler.so \
     vendor/samsung/gtaxllte/proprietary/lib/lib_SoundBooster_ver700.so:system/lib/lib_SoundBooster_ver700.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libaudio-ril.so:system/lib/libaudio-ril.so \
     vendor/samsung/gtaxllte/proprietary/lib/libaudioroute.so:system/lib/libaudioroute.so \
-    vendor/samsung/gtaxllte/proprietary/lib/libbt-vendor.so:system/lib/libbt-vendor.so \
     vendor/samsung/gtaxllte/proprietary/lib/libcodecdump.so:system/lib/libcodecdump.so \
     vendor/samsung/gtaxllte/proprietary/lib/libcsc.so:system/lib/libcsc.so \
     vendor/samsung/gtaxllte/proprietary/lib/libexynoscamera.so:system/lib/libexynoscamera.so \
     vendor/samsung/gtaxllte/proprietary/lib/libexynosgscaler.so:system/lib/libexynosgscaler.so \
     vendor/samsung/gtaxllte/proprietary/lib/libexynosscaler.so:system/lib/libexynosscaler.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libexynosutils.so:system/lib/libexynosutils.so \
     vendor/samsung/gtaxllte/proprietary/lib/libexynosv4l2.so:system/lib/libexynosv4l2.so \
     vendor/samsung/gtaxllte/proprietary/lib/libfloatingfeature.so:system/lib/libfloatingfeature.so \
     vendor/samsung/gtaxllte/proprietary/lib/libhwjpeg.so:system/lib/libhwjpeg.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libOpenCv.so:system/lib/libOpenCv.so \
     vendor/samsung/gtaxllte/proprietary/lib/libpreprocessing_nxp.so:system/lib/libpreprocessing_nxp.so \
     vendor/samsung/gtaxllte/proprietary/lib/libprotobuf-cpp-fooo.so:system/lib/libprotobuf-cpp-fooo.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
+    vendor/samsung/gtaxllte/proprietary/lib/librilutils.so:system/lib/librilutils.so \
     vendor/samsung/gtaxllte/proprietary/lib/libSamsungPostProcess.so:system/lib/libSamsungPostProcess.so \
     vendor/samsung/gtaxllte/proprietary/lib/libSamsungPostProcessConvertor.so:system/lib/libSamsungPostProcessConvertor.so \
     vendor/samsung/gtaxllte/proprietary/lib/libSamsungPreProcess.so:system/lib/libSamsungPreProcess.so \
-    vendor/samsung/gtaxllte/proprietary/lib/libsamsungRecord_ns.so:system/lib/libsamsungRecord_ns.so \
     vendor/samsung/gtaxllte/proprietary/lib/libsamsungRecord.so:system/lib/libsamsungRecord.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libsamsungRecord_ns.so:system/lib/libsamsungRecord_ns.so \
     vendor/samsung/gtaxllte/proprietary/lib/libsamsungSoundbooster_plus.so:system/lib/libsamsungSoundbooster_plus.so \
     vendor/samsung/gtaxllte/proprietary/lib/libsecaudioinfo.so:system/lib/libsecaudioinfo.so \
     vendor/samsung/gtaxllte/proprietary/lib/libseccameracore.so:system/lib/libseccameracore.so \
     vendor/samsung/gtaxllte/proprietary/lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/gtaxllte/proprietary/lib/libsensorlistener.so:system/lib/libsensorlistener.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libsensorservice.so:system/lib/libsensorservice.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libsomp.so:system/lib/libsomp.so \
     vendor/samsung/gtaxllte/proprietary/lib/libstainkiller.so:system/lib/libstainkiller.so \
     vendor/samsung/gtaxllte/proprietary/lib/libtinyalsa.so:system/lib/libtinyalsa.so \
     vendor/samsung/gtaxllte/proprietary/lib/libtinycompress.so:system/lib/libtinycompress.so \
     vendor/samsung/gtaxllte/proprietary/lib/libuniplugin.so:system/lib/libuniplugin.so \
+    vendor/samsung/gtaxllte/proprietary/lib/libvulkan.so:system/lib/libvulkan.so \
     vendor/samsung/gtaxllte/proprietary/lib/libwrappergps.so:system/lib/libwrappergps.so \
     vendor/samsung/gtaxllte/proprietary/lib/modules/qca_cld/qca_cld_wlan.ko:system/lib/modules/qca_cld/qca_cld_wlan.ko \
     vendor/samsung/gtaxllte/proprietary/lib/omx/libOMX.Exynos.AVC.Decoder.so:system/lib/omx/libOMX.Exynos.AVC.Decoder.so \
@@ -133,26 +141,12 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/gtaxllte/proprietary/vendor/lib/libLifevibes_lvvetx.so:system/vendor/lib/libLifevibes_lvvetx.so \
     vendor/samsung/gtaxllte/proprietary/vendor/lib/libMcClient.so:system/vendor/lib/libMcClient.so \
     vendor/samsung/gtaxllte/proprietary/vendor/lib/libMcRegistry.so:system/vendor/lib/libMcRegistry.so \
-    vendor/samsung/gtaxllte/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
-
-#PRODUCT_COPY_FILES += \
-#    vendor/samsung/gtaxllte/proprietary/bin/mcDriverDaemon:system/bin/mcDriverDaemon
-
-#PRODUCT_COPY_FILES += \
-#    vendor/samsung/gtaxllte/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
-
-#PRODUCT_COPY_FILES += \
-#    vendor/samsung/gtaxllte/proprietary/lib/libcamera_metadata.so:system/lib/libcamera_metadata.so
+    vendor/samsung/gtaxllte/proprietary/vendor/lib/libOpenCL.so:system/vendor/lib/libOpenCL.so \
+    vendor/samsung/gtaxllte/proprietary/vendor/lib/libSEF.so:system/vendor/lib/libSEF.so \
+    vendor/samsung/gtaxllte/proprietary/vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so
 
 PRODUCT_COPY_FILES += \
-    vendor/samsung/gtaxllte/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
-    vendor/samsung/gtaxllte/proprietary/lib/libaudio-ril.so:system/lib/libaudio-ril.so \
-    vendor/samsung/gtaxllte/proprietary/lib/libreference-ril.so:system/lib/libreference-ril.so \
-    vendor/samsung/gtaxllte/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
-    vendor/samsung/gtaxllte/proprietary/lib/librilutils.so:system/lib/librilutils.so \
+    vendor/samsung/gtaxllte/proprietary/lib/hw/camera.universal7870.so:system/lib/hw/camera.universal7870.so
 
 PRODUCT_COPY_FILES += \
-    vendor/samsung/gtaxllte/proprietary/bin/prepare_param.sh:system/bin/prepare_param.sh
-
-#    vendor/samsung/gtaxllte/proprietary/lib/libril.so:system/lib/libril.so \
-#    vendor/samsung/gtaxllte/proprietary/bin/rild:system/bin/rild \
+    vendor/samsung/gtaxllte/proprietary/lib/libbt-vendor.so:system/lib/libbt-vendor.so
